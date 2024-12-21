a46bece2a2b2ffaa428b23dc776320cc18735e5c
Konrad Wasowicz <exaroth@gmail.com>
2024-12-21 20:09:54 +0100

    Fix linter issues

--- liveboat.rb
+++ liveboat.rb
@@ -1,17 +1,17 @@
 # Homebrew configuration for liveboat distribution
- 
+
 class Liveboat < Formula
   desc "Static page generator for Newsboat feeds"
   homepage "https://github.com/exaroth/liveboat"
-  url  "https://github.com/exaroth/liveboat/releases/download/v0.9.0/liveboat-darwin"
+  url "https://github.com/exaroth/liveboat/releases/download/v0.9.0/liveboat-darwin"
   sha256 "e73df3d64108da48af7c3c3ba8fc9b049a77cff202ee77ad3bfae685ecd9b85b"
   version "0.9.0"
 
   def install
-	bin.install "liveboat-darwin" => "liveboat"
+    bin.install "liveboat-darwin" => "liveboat"
   end
 
   test do
-    system "#{bin}/liveboat --version"
+    system "#{bin}/liveboat" "--version"
   end
 end
