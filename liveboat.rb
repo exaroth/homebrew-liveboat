b8b6ec0eaf60de974c41367e3f00fc75ea66909e
Konrad Wasowicz <exaroth@gmail.com>
2024-12-21 20:42:23 +0100

    Remove trailing whitespace

--- liveboat.rb
+++ liveboat.rb
@@ -5,7 +5,7 @@ class Liveboat < Formula
   homepage "https://github.com/exaroth/liveboat"
   url "https://github.com/exaroth/liveboat/releases/download/v0.9.0/liveboat-darwin"
   version "0.9.0"
-  sha256 "f30e169f07f454e68a0eb19b8142f8abfe2d36db5564449073ffb79e2d8677cd" 
+  sha256 "f30e169f07f454e68a0eb19b8142f8abfe2d36db5564449073ffb79e2d8677cd"
 
   def install
     bin.install "liveboat-darwin" => "liveboat"
