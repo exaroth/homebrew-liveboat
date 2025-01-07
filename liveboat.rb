fac6c15086450c7eb8c2ba34bb690ec0f990ce66
Konrad Wasowicz <exaroth@gmail.com>
2025-01-07 03:30:39 +0100

    Bump version

--- liveboat.rb
+++ liveboat.rb
@@ -3,8 +3,8 @@
 class Liveboat < Formula
   desc "Static page generator for Newsboat feeds"
   homepage "https://github.com/exaroth/liveboat"
-  url "https://github.com/exaroth/liveboat/releases/download/v1.0.5/liveboat-darwin"
-  version "1.0.5"
+  url "https://github.com/exaroth/liveboat/releases/download/v1.0.7/liveboat-darwin"
+  version "1.0.7"
   sha256 "e06b53f5203b45f48bac747d15a7c3736896ae24bbe7c54ced7af02491088f03"
 
   def install
