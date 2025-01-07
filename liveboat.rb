80407b288aee502382abe5007dcb562c0aeb3010
Konrad Wasowicz <exaroth@gmail.com>
2025-01-07 03:32:44 +0100

    Update sha

--- liveboat.rb
+++ liveboat.rb
@@ -5,7 +5,7 @@ class Liveboat < Formula
   homepage "https://github.com/exaroth/liveboat"
   url "https://github.com/exaroth/liveboat/releases/download/v1.0.7/liveboat-darwin"
   version "1.0.7"
-  sha256 "e06b53f5203b45f48bac747d15a7c3736896ae24bbe7c54ced7af02491088f03"
+  sha256 "1bdd84425487c42df87a97f53ffb4685f8539aa7ef25bea77df1c4ab08311b86"
 
   def install
     bin.install "liveboat-darwin" => "liveboat"
